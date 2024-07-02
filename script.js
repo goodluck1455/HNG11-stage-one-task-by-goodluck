b17156208e989ad39f6c8473e93c8e18089fab76
goodluck1455 <goodluckstephen237@gmail.com>
2024-07-02 15:05:17 +0100

    new

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ function displayCurrentTime() {
   ];
   const now = new Date();
   const day = daysOfWeek[now.getUTCDay()]; 
-  const hours = String(now.getUTCHours()).padStart(2, "0");
-  const minutes = String(now.getUTCMinutes()).padStart(2, "0");
-  const seconds = String(now.getUTCSeconds()).padStart(2, "0");
+  const date = Date.now();
+//   const hours = String(now.getUTCHours()).padStart(2, "0");
+//   const minutes = String(now.getUTCMinutes()).padStart(2, "0");
+//   const seconds = String(now.getUTCSeconds()).padStart(2, "0");
 
-  
-  const timeString = `${day}, ${hours}:${minutes}:${seconds} UTC`;
+const timeString = `${day}, ${date} UTC`;
+//   const timeString = `${day}, ${date}:${minutes}:${seconds} UTC`;
 
   
   document.querySelector(".Current-time").textContent = timeString;
